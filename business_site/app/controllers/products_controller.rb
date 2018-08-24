cd18e961db2ee5771bac755301485ba920c1b345
Brandon Pitzen <bmpitzen@icloud.com>
2018-08-24 16:57:44 -0400

    Working code to parse data from CSV

--- business_site/app/controllers/products_controller.rb
+++ business_site/app/controllers/products_controller.rb
@@ -1,13 +1,27 @@
+require 'csv'
+
 class ProductsController < ApplicationController
   def home
   end
 
   def index
-    # this is pseudocode for what to do. Parse through csv and for each row, set eact attribute to the attribute in that row.
-    # header could be either a string or symbol
-    # @products = CSV.each_row do |row|
-    #   Product.new(pid: row[:pid], description: row[:description])
-    # end
+    @products = []
+    CSV.foreach 'lib/assets/faust_inventory.csv', 
+                headers: true, header_converters: :symbol do |row|
+      product = Product.new
+      product.pid = row.to_h[:pid]
+      product.item = row.to_h[:item]
+      product.description = row.to_h[:description]
+      product.price = row.to_h[:price]
+      product.condition = row.to_h[:condition]
+      product.dimension_w = row.to_h[:dimension_w]
+      product.dimension_l = row.to_h[:dimension_l]
+      product.dimension_h = row.to_h[:dimension_h]
+      product.img_file = row.to_h[:img_file]
+      product.quantity = row.to_h[:quantity]
+      product.category = row.to_h[:category]
+      @products << product
+    end
   end
 
   def show
--- business_site/app/models/product.rb
+++ business_site/app/models/product.rb
@@ -1,7 +1,8 @@
 class Product
   attr_accessor :pid, :item, :description, :price, :condition, :dimension_w,
                 :dimension_l, :dimension_h, :img_file, :quantity, :category
-
+  def initialize
+  end
   def clearance
   end
 
