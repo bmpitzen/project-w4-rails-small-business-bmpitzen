9aad6632e6bc9861864f71dae68f9cc8d4132a2f
Brandon Pitzen <bmpitzen@icloud.com>
2018-08-26 16:24:53 -0400

    Unsuccessful attempt to display product info on page.
    Still working on getting this working but I wanted to commit some of my changes before continuing on.

--- business_site/app/controllers/products_controller.rb
+++ business_site/app/controllers/products_controller.rb
@@ -1,26 +1,22 @@
 require 'csv'
 
 class ProductsController < ApplicationController
+  attr_accessor :product_box
   def home
   end
 
   def index
-    @products = []
-    CSV.foreach 'lib/assets/faust_inventory.csv', 
-                headers: true, header_converters: :symbol do |row|
-      product = Product.new
-      product.pid = row.to_h[:pid]
-      product.item = row.to_h[:item]
-      product.description = row.to_h[:description]
-      product.price = row.to_h[:price]
-      product.condition = row.to_h[:condition]
-      product.dimension_w = row.to_h[:dimension_w]
-      product.dimension_l = row.to_h[:dimension_l]
-      product.dimension_h = row.to_h[:dimension_h]
-      product.img_file = row.to_h[:img_file]
-      product.quantity = row.to_h[:quantity]
-      product.category = row.to_h[:category]
-      @products << product
+  end
+
+  def product_box
+    @pinfo = []
+    product.pdata.each do |item|
+      info = %(
+        #{item.item}
+        #{item.description} 
+        #{item.price}
+        )
+      @pinfo << info
     end
   end
 
