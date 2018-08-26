65aada0fd0faccf30325dd0e6c320c5de0a101ea
Brandon Pitzen <bmpitzen@icloud.com>
2018-08-26 17:28:05 -0400

    Working information displaying on webpage.

--- business_site/app/controllers/products_controller.rb
+++ business_site/app/controllers/products_controller.rb
@@ -1,25 +1,36 @@
+# frozen_string_literal: true
+
 require 'csv'
 
 class ProductsController < ApplicationController
-  attr_accessor :product_box
-  def home
-  end
+  def home; end
 
   def index
+    @products = data
   end
-
-  def product_box
-    @pinfo = []
-    product.pdata.each do |item|
-      info = %(
-        #{item.item}
-        #{item.description} 
-        #{item.price}
-        )
-      @pinfo << info
-    end
-  end
-
   def show
+  end
+
+  private
+
+  def data
+    product_array = []
+    CSV.foreach 'lib/assets/faust_inventory.csv',
+                headers: true, header_converters: :symbol do |row|
+      product = Product.new
+      product.pid = row.to_h[:pid]
+      product.item = row.to_h[:item]
+      product.description = row.to_h[:description]
+      product.price = row.to_h[:price]
+      product.condition = row.to_h[:condition]
+      product.dimension_w = row.to_h[:dimension_w]
+      product.dimension_l = row.to_h[:dimension_l]
+      product.dimension_h = row.to_h[:dimension_h]
+      product.img_file = row.to_h[:img_file]
+      product.quantity = row.to_h[:quantity]
+      product.category = row.to_h[:category]
+      product_array << product
+    end
+    return product_array
   end
 end
--- business_site/app/models/product.rb
+++ business_site/app/models/product.rb
@@ -2,30 +2,9 @@ class Product
   require 'csv'
   attr_accessor :pid, :item, :description, :price, :condition, :dimension_w,
                 :dimension_l, :dimension_h, :img_file, :quantity, :category,
-                :data, :product_box, :products
+                :product_box, :products
   def initialize
-    @product = Product.new
-    @pdata = []
   end
   def clearance
-  end
-  def create
-  end
-  def data
-    CSV.foreach 'lib/assets/faust_inventory.csv',
-                headers: true, header_converters: :symbol do |row|
-      @product.pid = row.to_h[:pid]
-      @product.item = row.to_h[:item]
-      @product.description = row.to_h[:description]
-      @product.price = row.to_h[:price]
-      @product.condition = row.to_h[:condition]
-      @product.dimension_w = row.to_h[:dimension_w]
-      @product.dimension_l = row.to_h[:dimension_l]
-      @product.dimension_h = row.to_h[:dimension_h]
-      @product.img_file = row.to_h[:img_file]
-      @product.quantity = row.to_h[:quantity]
-      @product.category = row.to_h[:category]
-      @pdata << product
-    end
   end
 end
